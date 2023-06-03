6d6c0447be40921d8fc981bf2f52cdceebb5a163
fiqriachmada <achmada@1998.com>
2023-06-03 15:51:25 +0700

    new

--- app/(default)/about/page.js
+++ app/(default)/about/page.js
@@ -1,31 +1,30 @@
-'use client';
+'use client'
 
-import React from 'react';
-import aboutItem from './aboutItems';
-import aboutItems from './aboutItems';
+import React from 'react'
+import aboutItems from './aboutItems'
 
-function About() {
+function About () {
   return (
     <div>
-      {aboutItems.map((aboutItem) => (
-        <div className="bg-white text-black shadow-xl rounded-md">
-          <div className="p-6 lg:px-32">
-            <div className="flex justify-center pt-5 capitalize">
+      {aboutItems.map(aboutItem => (
+        <div className='bg-white text-black shadow-xl rounded-md'>
+          <div className='p-6 lg:px-32'>
+            <div className='flex justify-center pt-5 capitalize'>
               {aboutItem.welcome}
             </div>
-            <div className="flex justify-center pt-5">
+            <div className='flex justify-center pt-5'>
               <img
-                src="https://svgshare.com/i/tmn.svg"
-                className="hover:bg-slate-300 hover:rounded-2xl"
+                src='https://svgshare.com/i/tmn.svg'
+                className='hover:opacity-70'
               />
             </div>
-            <div className="text-center pt-5 ">{aboutItem.content[1]}</div>
-            <div className="text-center pt-5 ">{aboutItem.content[2]}</div>
+            <div className='text-center pt-5 '>{aboutItem.content[1]}</div>
+            <div className='text-center pt-5 '>{aboutItem.content[2]}</div>
           </div>
         </div>
       ))}
     </div>
-  );
+  )
 }
 
-export default About;
+export default About
--- app/(default)/layout.jsx
+++ app/(default)/layout.jsx
@@ -1,14 +1,5 @@
 "use client";
 
-// import Header from "./header";
-// import Footer from "./footer";
-
 export default function DefaultLayout({ children }) {
-  return (
-    <main>
-      {/* <div className=" mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 "> */}
-      {children}
-      {/* </div> */}
-    </main>
-  );
+  return <main>{children}</main>;
 }

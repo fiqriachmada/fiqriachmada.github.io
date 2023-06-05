a6d052e381cd9bc96c6b014065408f69d72292c5
fiqriachmada <achmada1998@gmail.com>
2023-06-05 11:30:45 +0700

    new

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import Footer from './(default)/footer';
 import Header from './(default)/header';
 import './globals.css';
 import { Plus_Jakarta_Sans } from 'next/font/google';
-import favicon from './../public/favicon.svg';
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
@@ -12,13 +11,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  console.log('favicon', favicon);
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href={favicon.src} />
-        {/* <link rel="icon" href="./../public/favicon.svg" /> */}
-      </head>
       <body className={`${jakarta.className}`}>
         <div className="bg-gray-800 mb-16">
           <div className=" mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">

2f016a7e3a9f02ec3531f20bed1c651e85022805
fiqriachmada <achmada1998@gmail.com>
2023-06-05 12:01:47 +0700

    add google verif

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import Header from './(default)/header';
 import './globals.css';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 
+// import google_verification from './../public/googlec4f6b23b5640dd6a.html';
+
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export const metadata = {
@@ -11,8 +13,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  // console.log('google_verification', google_verification);
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="E9my8MvFE40cVa4KkhP3LP9b6XHVcsAc7oFbi5S3EPs"
+        />
+      </head>
       <body className={`${jakarta.className}`}>
         <div className="bg-gray-800 mb-16">
           <div className=" mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">

7d66f7cc4e49c6e3ee4b58b934cb7996bb4701bb
fiqriachmada <achmada1998@gmail.com>
2023-06-16 17:17:47 +0700

    new condition

--- app/(default)/project/page.js
+++ app/(default)/project/page.js
@@ -12,16 +12,20 @@ function Project() {
     <div className="grid lg:grid-cols-2 gap-4 grid-cols-1">
       {projectItems.map((projectItem) => (
         <div className="bg-white text-black shadow-xl rounded-md mt-5">
+          <div className="flex justify-center">
+            <a href={projectItem.link} target="_blank">
+              <img
+                src={projectItem.image}
+                className="hover:opacity-70 object-fill rounded-t-md"
+              />
+            </a>
+          </div>
           <div className="p-6 md::px-10">
             <>
-              <div className="flex justify-center pt-5">
-                <img
-                  src={projectItem.image}
-                  className="hover:opacity-70 object-fill"
-                />
-              </div>
               <div className="flex justify-start pt-5 capitalize">
-                {projectItem.title}
+                <a href={projectItem.link} target="_blank">
+                  {projectItem.title}
+                </a>
               </div>
               {/* <div className='text-center pt-5 '>{projectItem.content[1]}</div>
               <div className='text-center pt-5 '>{projectItem.content[2]}</div> */}

40b22307e87a014aae36fc207b309fa71308fdaf
fiqriachmada <achmada1998@gmail.com>
2024-01-12 17:37:04 +0700

    fixed

--- app/(default)/work/page.jsx
+++ app/(default)/work/page.jsx
@@ -7,18 +7,18 @@ function Work() {
   return (
     <div>
       Work History
-      <div className="flex-col space-y-10 my-10">
+      <div className=" grid grid-cols-1 space-y-10 my-10">
         {workItems.map((workItem) => (
-          <div className="bg-white text-black rounded-lg">
+          <div className="flex items-center bg-white text-black rounded-lg p-12 gap-3">
             <img src={workItem.image} style={{ width: 400, height: 400 }} />
-            <div className="px-10 py-10 ">
-              Company Name:
+            <div className="px-10 py-10 space-y-2">
+              <div className="">Company Name:</div>
               <div>{workItem.company}</div>
-              Start Date:
+              <div className="">Start Date:</div>
               <div>{workItem.startDate}</div>
-              End Date:
+              <div className="">End Date:</div>
               <div>{workItem.endDate}</div>
-              Title:
+              <div className="">Title:</div>
               <div>{workItem.title}</div>
             </div>
           </div>

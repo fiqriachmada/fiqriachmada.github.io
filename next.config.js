87fa987337296754653a120337769314da7cc724
fiqriachmada <achmada1998@gmail.com>
2024-01-15 14:38:41 +0700

    fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { forceSwcTransforms: true },
-  output: 'standalone',
+  output: "export",
+  
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;

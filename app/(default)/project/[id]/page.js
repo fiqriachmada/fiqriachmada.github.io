9a44bed485858190b94074b8a4a3f72631abc80e
fiqriachmada <achmada1998@gmail.com>
2024-01-15 17:39:46 +0700

    fixed

--- app/(default)/project/[id]/page.js
+++ app/(default)/project/[id]/page.js
@@ -14,6 +14,7 @@ export function generateStaticParams() {
 
 function Detail(params) {
   const id = params.params.id;
+  
 
   const detailItem = projectItems.find((item) => item.id.toString() === id);
 

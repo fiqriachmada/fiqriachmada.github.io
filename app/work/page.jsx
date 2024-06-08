bd4060fc18faed8d568535f4afe9601718de9c00
fiqriachmada <achmada@1998.com>
2024-06-08 18:54:29 +0700

    fixed

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,30 @@
+import WorkCard from "@/components/work/workCard";
 import { fetchWorkData } from "@/lib/workApi";
 import React from "react";
 
+
+// async function getWorkDataData() {
+//   const cookieData = cookies().getAll();
+//   return new Promise((resolve) =>
+//     setTimeout(() => {
+//       resolve(cookieData);
+//     }, 1000)
+//   );
+// }
+// async function getCookieData() {
+//   const fetchWorkDataPromise = fetchWorkData().catch((error) => {
+//     console.log("Error fetching work data:", error);
+//     return []; // Return empty array if fetch fails
+//   });
+
+//   // Wait for both cookieData and fetchWorkData to resolve
+//   const [workData] = await Promise.all([fetchWorkDataPromise]);
+
+//   return { workData };
+// }
+
 async function page() {
+//   const { cookies, workData } = await getCookieData();
   var data = [];
   try {
     data = await fetchWorkData();
@@ -11,7 +34,10 @@ async function page() {
   return (
     <div>
       page
+      {/* <pre>{JSON.stringify(cookies, null, 2)}</pre>
+      <pre>{JSON.stringify(workData, null, 2)}</pre> */}
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <WorkCard data={data.data} />
     </div>
   );
 }

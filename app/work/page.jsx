c41ba5229a1ec0b610e657ceeabb5c6a0c6b90ac
fiqriachmada <achmada@1998.com>
2024-06-08 18:25:53 +0700

    fixed

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { fetchWorkData } from "@/lib/workApi";
+import React from "react";
 
-function page() {
+async function page() {
+  var data = [];
+  try {
+    data = await fetchWorkData();
+  } catch (error) {
+    console.log("error", error);
+  }
   return (
-    <div>page</div>
-  )
+    <div>
+      page
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
-export default page
+export default page;

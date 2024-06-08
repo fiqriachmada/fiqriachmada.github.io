0165af454d2e077a170e1d2dad5d7447ecfcf496
fiqriachmada <achmada@1998.com>
2024-06-08 16:40:12 +0700

    fixed

--- app/layout.js
+++ app/layout.js
@@ -1,38 +1,32 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
-import "./globals.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import Footer from '../components/footer'
+import Header from '../components/header'
+import './globals.css'
+import { Plus_Jakarta_Sans } from 'next/font/google'
 
 // import google_verification from './../public/googlec4f6b23b5640dd6a.html';
 
-const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "fiqriachmada",
-  description: "personal site",
-};
+  title: 'fiqriachmada',
+  description: 'personal site'
+}
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <meta
-          name="google-site-verification"
-          content="E9my8MvFE40cVa4KkhP3LP9b6XHVcsAc7oFbi5S3EPs"
+          name='google-site-verification'
+          content='E9my8MvFE40cVa4KkhP3LP9b6XHVcsAc7oFbi5S3EPs'
         />
       </head>
       <body className={`${jakarta.className}`}>
-        <div className="fixed top-0 right-0 left-0 z-10 mx-8 lg:mx-40 my-5 bg-gray-800 mb-16 rounded-xl hover:scale-[101%]">
+        <div className='fixed top-0 right-0 left-0 z-10 mx-6 lg:mx-28 my-5 bg-gray-800 mb-16 rounded-xl hover:scale-[101%]'>
           <Header />
         </div>
-        <div className="mx-8 lg:mx-40 my-24 lg:my-28">{children}</div>
-
-        {/* <div className="bg-gray-600 mt-16">
-          <div className=" mx-auto max-w-7xl px-4 sm:px-8 lg:px-10">
-            <Footer />
-          </div>
-        </div> */}
+        <div className='mx-6 lg:mx-28 my-24 lg:my-28'>{children}</div>
       </body>
     </html>
-  );
+  )
 }
